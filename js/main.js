95e25ccf6292fa4517d77f4e6a7df21f4160edf4
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-10-26 19:29:54 -0400

    remove commented out code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const outputArea = document.querySelector(".textbox");
 const inputArea = document.querySelector(".input-area");
 const triangle = document.querySelector(".triangle");
 document.querySelector(".submit").addEventListener("click", randomPhrase);
-// document.querySelector(".submit").addEventListener("click", removeClass);
 document.querySelector(".submit").addEventListener("click", addEffect);
 
 function randomPhrase() {
@@ -66,13 +65,6 @@ function randomPhrase() {
 }
 
 function addEffect() {
-  // if ((triangle.classList.contains("triangle-result") && outputArea.classList.contains("text-result")) && (triangle.classList.contains("triangle") && outputArea.classList.contains("textbox"))) {
-  //   triangle.classList.remove("triangle-result");
-  //   outputArea.classList.remove("text-result");
-  // } else {
-  //   triangle.classList.add("triangle-result");
-  //   outputArea.classList.add("text-result");
-  // }
   triangle.classList.toggle("triangle-result");
   outputArea.classList.toggle("text-result");
   const timeout = function() {
@@ -85,52 +77,3 @@ function resetEffect() {
   triangle.classList.toggle("triangle-result");
   outputArea.classList.toggle("text-result");
 }
-
-
-
-
-
-
-// function test() {
-//   outputArea.innerText = "Hello World";
-// }
-// test()
-
-// const eightball = {
-//   output: document.querySelector(".textbox"),
-//   test() {
-//     this.output.innerHTML = "test";
-//   }
-// }
-// eightball.test
-// var words = [
-//   'It is certain',
-// 'It is decidedly so',
-// 'Without a doubt',
-// 'Yes definitely',
-// 'You may rely on it',
-// 'As I see it, yes',
-// 'Most likely',
-// 'Outlook good',
-// 'Yes',
-// 'Signs point to yes',
-// 'Reply hazy try again',
-// 'Ask again later',
-// 'Better not tell you now',
-// 'Cannot predict now',
-// 'Concentrate and ask again',
-// 'Do not count on it',
-// 'My reply is no',
-// 'My sources say no',
-// 'Outlook not so good',
-// 'Very doubtful'
-//   ];
-
-//   var getRandomWord = function () {
-//     return words[Math.floor(Math.random() * words.length)];
-//   };
-// $(function() {$('.eball').mouseenter(function(){
-//   $('.textbox').html(getRandomWord());
-//   });
-
-// });
