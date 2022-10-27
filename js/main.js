5930aae4b51b47552590969ee66fe339fef81775
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-10-26 22:31:00 -0400

    added shake effect, but i want it to happen before results shown

--- js/main.js
+++ js/main.js
@@ -2,18 +2,25 @@
 const outputArea = document.querySelector(".textbox");
 const inputArea = document.querySelector(".input-area");
 const triangle = document.querySelector(".triangle");
+const ball = document.querySelector(".ball");
 document.querySelector(".ball").addEventListener("click", addEffect);
 
+// function shakeIt () {
+//   ball.classList.add("shake");
+//   addEffect();
+// }
 
 function addEffect() {
   if (triangle.classList.contains("triangle-result")  && outputArea.classList.contains("text-result")) {
     triangle.classList.remove("triangle-result");
     outputArea.classList.remove("text-result");
+    ball.classList.remove("shake");
     console.log("classes removed");
     effectTimeout();
   } else {
     triangle.classList.add("triangle-result");
     outputArea.classList.add("text-result");
+    ball.classList.add("shake");
     console.log("classes added");
     randomPhrase();
   }
@@ -27,6 +34,7 @@ function effectTimeout() {
 function resetEffect() {
   triangle.classList.toggle("triangle-result");
   outputArea.classList.toggle("text-result");
+  ball.classList.toggle("shake");
   randomPhrase();
 }
 
